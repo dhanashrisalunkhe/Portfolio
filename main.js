8aeebc8df243c2dab9681bc781d7aab3869c19a9
dhanashrisalunkhe <99960788+dhanashrisalunkhe@users.noreply.github.com>
2024-06-20 12:59:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: ["E&TC Engineer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
